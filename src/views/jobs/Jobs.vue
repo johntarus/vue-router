93b76d5f79d299680bcfd129aee194ae6b2729fd
tarus <tarusjohn96@gmail.com>
2022-11-09 14:21:05 +0300

    feat: fetched data using json server

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
+    <div v-if="jobs.length">
     <h1 class="flex justify-center font-bold text-4xl">Jobs</h1>
     <div v-for="job in jobs" :key="job.id" class="flex justify-center" >
         <RouterLink :to="{name: 'JobsDetails', params: {id: job.id}}">
             <h2>{{job.title}}</h2>
         </RouterLink>
+    </div>
+    </div>
+    <div v-else class="flex justify-center">
+        <p>Loading Jobs</p>
     </div>
 </template>
 
@@ -11,13 +16,13 @@
 export default {
     data() {
         return {
-            jobs: [
-                    {title: "Frontend Developer", details: "competitive role", id: 1},
-                    {title: "Backend Developer", details: "competitive role", id: 2},
-                    {title: "Data Engineer", details: "competitive role", id: 3},
-                    {title: "DevOps Engineer", details: "competitive role", id: 4}
-                ],
+            jobs: [],
             }
+    },
+    mounted() {
+        fetch("http://localhost:3000/jobs").then(response => response.json())
+            .then(data => this.jobs = data)
+            .catch(error=> console.log(error.message))
         }
     }
 </script>
--- src/views/jobs/JobsDetails.vue
+++ src/views/jobs/JobsDetails.vue
@@ -1,9 +1,26 @@
 <template>
-    <h2 class="flex justify-center">The job is {{id}}</h2>
+    <div v-if="job" class="flex flex-col justify-center items-center">
+    <h1>{{job.title}}</h1>
+    <h2>The job is {{id}}</h2>
+    <h1>{{job.detail}}</h1>
+    </div>
+    <div v-else class="flex justify-center">
+        <p>Loading Job Details...</p>
+    </div>
 </template>
 
 <script>
 export default {
-        props: ['id']
+    props: ['id'],
+    data() {
+        return {
+            job: null,
+        }
+    },
+    mounted() {
+        fetch("http://localhost:3000/jobs/" + this.id).then(response => response.json())
+            .then(data => this.job = data)
+            .catch(error=> console.log(error.message))
+        }
     }
 </script>
